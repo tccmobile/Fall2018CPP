fa6319da00a3797fea00e9d1be20f6e484c592d4
drwillsmith <drwillsmith@gmail.com>
2018-08-30 08:46:42 -0500

    Chapter 1 Warmup

---
+++ Ch1Warmup.cpp
@@ -0,0 +1,17 @@
+//
+// Created by William Smith on 8/30/18.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int userNum;
+
+    cout << "Enter integer:" << endl;
+    cin  >> userNum;
+
+    /* Type your code here */
+
+    return 0;
+}
