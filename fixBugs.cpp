d87df8df8420377435e6cc0c5928afd92104f70e
drwillsmith <drwillsmith@gmail.com>
2018-08-30 08:38:29 -0500

    Initial commit

---
+++ fixBugs.cpp
@@ -0,0 +1,22 @@
+//
+// Created by William Smith on 8/30/18.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numBeans
+    int numJars;
+    inl totalBeans;
+
+    numBeans = 500;
+    numJars = 3;
+
+    cout << numBeans << " beans in ";
+    cout << numJar   << " jars yields ";
+    totalBeans = numBeans * numJars;
+    cout << totalBeans " total" endl;
+
+    return 0;
+}
